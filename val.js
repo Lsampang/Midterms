0b7f6f6afa7f134f190711f2e4546f47586b48ed
Lorenzo Sampang <SAMPANG.LORENZOGABRIEL@UE.EDU.PH>
2020-10-11 15:48:40 +0800

    Added validation lines

--- val.js
+++ val.js
@@ -124,6 +124,87 @@ function isLetter() {
   }
 }
 
+function First() {
+  var firstNameValue = document.getElementById("firstName").value;
+  let alp1 = /[A-Za-z]/;
+  if (alp1.test(firstNameValue) == true) {
+    return true;
+  }
+  else{
+    alert("Alphabet lang hoy!!!");
+    return false;
+  }
+}
+
+function MI() {
+  var MiddleValue = document.getElementById("Middle").value;
+  let alp2 = /[A-Za-z]/;
+  if (alp2.test(MiddleValue) == true) {
+    return true;
+  }
+  else{
+    alert("Alphabet lang hoy!!!");
+    return false;
+  }
+}
+function StudentNo() {
+  var StudentNumberValue = document.getElementById("StudentNumber").value;
+  let number = /[0-9]/;
+  if (number.test(StudentNumberValue) == true) {
+    return true;
+  }
+  else{
+    alert("Numbers lang Hoy!!!");
+    return false;
+  }
+}
+function Pass() {
+  var PasswordValue = document.getElementById("Password").value;
+  let pass = /[a-zA-Z0-9]/;
+  if (pass.test(PasswordValue) == true) {
+    return true;
+  }
+  else{
+    alert("Alphanumeric lang pwede!!!");
+    return false;
+  }
+}
+
+function User() {
+  var UsernameValue = document.getElementById("Username").value;
+  let user = /[A-Za-z\-\_]/;
+  if (user.test(UsernameValue) == true) {
+    return true;
+  }
+  else{
+    alert("Dash and Underscore lang pwede Hoy!!!")
+    return false;
+  }
+}
+
+function Phone(){
+  var cpNumberValue = document.getElementById("cpNumber").value;
+  let no = /[0-9\+]/;
+  if (no.test(cpNumberValue) == true) {
+    return true;
+  }
+  else{
+    alert("Mali yan kapatid!!!");
+    return false;
+  }
+}
+function Bday(){
+  var BirthdayValue = document.getElementById("Birthday").value;
+  let day =/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+  if (day.test(BirthdayValue) <= 2003) {
+    alert("You must be 18 years old and above");
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
 /*
 function phonenumber(str){
         var phoneno = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
